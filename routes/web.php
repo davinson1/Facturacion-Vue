1c9bd534b79cae511182de210a10f146b0f3f346
Davinson Rocha Salazar <davinson.rochas@gmail.com>
2020-11-08 21:04:31 -0500

    crud de departamentos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Ubicacion\DepartamentoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Ubicacion\PaisController;
 
@@ -25,8 +26,15 @@ Route::middleware(['auth'])->group(function(){
 	Route::get('/pais', [PaisController::class, 'index']);
 	Route::get('/listar_paises', [PaisController::class, 'listar']);
 	Route::post('/pais_crear', [PaisController::class, 'create']);
-
 	Route::delete('/paises_eliminar/{id}', 'Ubicacion\PaisController@destroy');
 	Route::put('/pais_editar', 'Ubicacion\PaisController@update');
+
+
+  Route::get('/departamento', [DepartamentoController::class, 'index']);
+  Route::get('/listar_departamentos', [DepartamentoController::class, 'listar']);
+  Route::post('/departamento_crear', [DepartamentoController::class, 'create']);
+  Route::get('/select_pais', [DepartamentoController::class, 'selectPais']);
+  Route::delete('/departamento_eliminar/{id}', 'Ubicacion\DepartamentoController@destroy');
+  Route::put('/departamento_editar', 'Ubicacion\DepartamentoController@update');
 
 });
